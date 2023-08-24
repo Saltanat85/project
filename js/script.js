a5ebab85c83e8215de5374eb797835160b036e12
Saltanat <example@gmail.com>
2023-08-24 14:20:20 +0600

    third

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
-let incr =10;
-    decr=10;
+const numberOfFilms=+prompt('Сколько фильмов  Вы просмотрели?', '');
 
-    incr++;
-    decr--;
+const personalMovieDB={
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    geners:[],
+    privat: false
+};
+const a =prompt('Один из последних просмотренных фильмов?', ''),
+      b =prompt('На сколько оцените его?', ''),
+      c =prompt('Один из последних просмотренных фильмов?', ''),
+      d =prompt('На сколько оцените его?', '');
 
-    console.log(incr);
-    console.log(decr);
-
-
+      personalMovieDB.movies[a]=b;
+      personalMovieDB.movies[c]=d;
+      console.log(personalMovieDB)
